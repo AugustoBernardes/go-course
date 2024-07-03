ec5bd0bfe190dab37399d8ff7224aeedba284dfb
Augusto Bernardes <augustobdev@outlook.com>
2024-07-03 08:24:48 -0300

    feat: file server

---
+++ Important-packages/8-file-server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./public"))
+	mux := http.NewServeMux()
+	mux.Handle("/test", fileServer)
+	mux.HandleFunc("/blog", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello blog"))
+	})
+	http.ListenAndServe(":8080", mux)
+
+}
